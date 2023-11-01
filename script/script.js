225cee60fb4df5d7ea2df72bb68632b1563e4cf0
john ormartii <johnormartii@gmail.com>
2023-11-01 20:13:11 +0300

    second commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,12 @@
-scripts/script.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtqZbu6mKk0C2LuM3Q-KJWdhc7VGWYwcU",
+  authDomain: "jonte-todo-app.firebaseapp.com",
+  databaseURL: "https://jonte-todo-app-default-rtdb.firebaseio.com",
+  projectId: "jonte-todo-app",
+  storageBucket: "jonte-todo-app.appspot.com",
+  messagingSenderId: "114281798224",
+  appId: "1:114281798224:web:4276a6805a6efe1be78f55",
+  measurementId: "G-F511660P8B"
+};
+
